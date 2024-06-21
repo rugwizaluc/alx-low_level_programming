af3cc713fde9bafeda5f41cff2826d2ea54d2597
rugwizaluc <rugwizajeanluc123@gmail.com>
2024-06-21 16:54:20 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int number;
+
+    for (number = 0; number < 10; number++)
+    {
+    printf("%i", number);
+    }
+    putchar('\n');
+
+    return (0);
+}
+    

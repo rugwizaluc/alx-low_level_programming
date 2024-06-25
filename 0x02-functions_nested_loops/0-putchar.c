6c8ec3910d9f8c643c4d1e4777e8d622b7f9fa68
rugwizaluc <rugwizajeanluc123@gmail.com>
2024-06-25 15:52:15 -0700

    putchar 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Print the word "ALX"
+* main - Print the word "_putchar"
 *
 * Return: Always 0 (Success)
 */
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int i;
-	char p[] = "ALX";
+	char p[] = "_putchar";
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 8; i++)
 		{
 		_putchar(p[i]);
 		}

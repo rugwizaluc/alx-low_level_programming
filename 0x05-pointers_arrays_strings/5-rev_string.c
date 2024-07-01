fa3e6a03cffa0ec4e82f49f31e581083d8e70254
rugwizaluc <rugwizajeanluc123@gmail.com>
2024-07-01 16:06:44 -0700

    pointers and arrays 05

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,30 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
-   * main - check the code for My School
-    *
-     * Return: Always 0.
-      */
-int main(void)
+ *rev_string - update value.
+ *@s: value to be evaluate.
+ *Return: not.
+ */
+void rev_string(char *s)
 {
-     char s[10] = "My School";
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
 
-     printf("%s\n", s);
-     rev_string(s);
-     printf("%s\n", s);
-     return (0);
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for ( ; e < ((l / 2) + 1) ; e++)
+	{
+		x = (l - e);
+		n = s[e];
+		s[e] = s[x];
+		s[x] = n;
+	}
 }

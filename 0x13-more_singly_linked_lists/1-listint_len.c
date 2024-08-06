1153f17de6205d5f8ad4b0c1a045ae6a6646b755
rugwizaluc <rugwizajeanluc123@gmail.com>
2024-08-06 15:29:52 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,10 +7,10 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; h; i++)
-        h = h->next;
+	for (i = 0; h; i++)
+		h = h->next;
 
-    return (i);
+	return (i);
 }
